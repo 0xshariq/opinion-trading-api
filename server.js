f42435bc1958d5736663bcd2d51c0ebe06ff1b0e
Shariq Choudhary <khanshariq92213@gmail.com>
2025-03-13 23:13:20 +0530

    MInor changes

--- server.js
+++ server.js
@@ -55,4 +55,4 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Stock Trading API");
 });
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
